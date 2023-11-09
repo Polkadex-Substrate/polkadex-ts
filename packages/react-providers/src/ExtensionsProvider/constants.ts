c256400311a8b7d7a7e1f704938871a5181c702a
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2023-11-09 16:42:03 +0530

    add extensions provider

---
+++ packages/react-providers/src/ExtensionsProvider/constants.ts
@@ -0,0 +1,11 @@
+import {ExtensionsContextInterface} from "./types";
+
+export const defaultExtensionsContext: ExtensionsContextInterface = {
+    checkingInjectedWeb3: false,
+    extensionsStatus: {},
+    setExtensionStatus: (id, status) => {},
+    removeExtensionStatus: (id) => {},
+    extensionInstalled: (id) => false,
+    extensionCanConnect: (id) => false,
+    extensionHasFeature: (id, feature) => false,
+};
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,4 +1,5 @@
 import {AnyFunction, AnyJson} from "./types";
+import type { MutableRefObject } from "react";
 
 export const withTimeout = (fn: AnyFunction, args: AnyJson, timeout: number) => {
     return new Promise((resolve, reject) => {
@@ -7,4 +8,17 @@ export const withTimeout = (fn: AnyFunction, args: AnyJson, timeout: number) => 
             reject();
         }, timeout);
     });
+};
+
+/**
+ * @name setStateWithRef
+ * @summary Synchronize React state and its reference with the provided value.
+ */
+export const setStateWithRef = <T>(
+    value: T,
+    setState: (_state: T) => void,
+    ref: MutableRefObject<T>
+): void => {
+    setState(value);
+    ref.current = value;
 };
