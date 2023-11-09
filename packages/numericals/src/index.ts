997f2644c84d7fa43cdd4d8e6c9c54709f84a0df
Arjun Porwal <65214523+ap211unitech@users.noreply.github.com>
2023-11-09 15:11:56 +0530

    update: added some utility functions

--- packages/numericals/src/index.ts
+++ packages/numericals/src/index.ts
@@ -1,7 +1,69 @@
+import { TrimFloatProps } from "./types";
+
 export const add = (a: number, b: number) => {
-    return a + b;
+  return a + b;
 };
 
 export const subtract = (a: number, b: number) => {
-    return a - b;
+  return a - b;
+};
+
+// @Param - string
+// @Desc - Removes the trailing zeros
+export const formatNumber = (value: string): string => {
+  return value
+    .replace(/(\.\d*?)0+$/, "$1") // Remove trailing zeros after the decimal
+    .replace(/\.$/, ""); // Remove the deciaml point if there are no decimal places)
+};
+
+// @Param - string/number
+// @Desc - Trim the value till given decimal places (Prevent's roundoff)
+export const trimFloat = ({
+  value,
+  digitsAfterDecimal = 8,
+}: TrimFloatProps): string => {
+  const valueString = value.toString();
+  const decimalIndex = valueString.indexOf(".");
+
+  if (decimalIndex !== -1) {
+    const numberPart = valueString.substr(
+      0,
+      decimalIndex + digitsAfterDecimal + 1
+    );
+    return formatNumber(parseFloat(numberPart).toFixed(digitsAfterDecimal));
+  }
+
+  return formatNumber(valueString);
+};
+
+// @Param - string/number
+// @Desc - Get count of digits after decimal
+export const decimalPlaces = (num: number | string) => {
+  const match = ("" + num).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
+  if (!match) {
+    return 0;
+  }
+  return Math.max(
+    0,
+    // Number of digits right of decimal point.
+    (match[1] ? match[1].length : 0) -
+      // Adjust for scientific notation.
+      (match[2] ? +match[2] : 0)
+  );
+};
+
+// @Param - string/number
+// @Desc - Removes the commas from a string
+export const removeCommas = (value: string | number): string => {
+  const valueStr = value.toString();
+  return valueStr.replace(/,/g, "");
+};
+
+// @Param - string (could be float or int)
+// @Desc - Check if number contains only zeros (i.e. 0.000000)
+export const hasOnlyZeros = (floatString: string): boolean => {
+  const floatValue = parseFloat(floatString);
+  const integerValue = parseInt(floatString, 10);
+
+  return floatValue === integerValue;
 };
