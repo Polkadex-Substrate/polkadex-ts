173e3d0377c37e3498cd10c8c8dd9280a890a65c
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2023-11-10 14:22:13 -0300

    update: thea project

---
+++ apps/web/tailwind.config.ts
@@ -0,0 +1,19 @@
+import path from "path";
+
+import type { Config } from "tailwindcss";
+
+import { themeConfig } from "../../themeConfig";
+
+export const config: Config = {
+  ...themeConfig,
+  content: [
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    path.join(
+      path.dirname(require.resolve("@polkadex/ux")),
+      "**/*.{js,ts,jsx,tsx,mdx}"
+    ),
+  ],
+};
+
+export default config;
