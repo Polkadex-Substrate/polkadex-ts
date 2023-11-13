da3d88441484d1a65c30b454eae375e074dec304
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2023-11-13 14:15:41 +0530

    undo apps change

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 import Image from "next/image";
 import {
-  ExtensionsProvider,
   useExtensions,
-  ExtensionAccountsProvider,
 } from "@polkadex/react-providers";
 
 import styles from "./page.module.css";
+import {Card} from "ui";
 
 function Gradient({
   conic,
@@ -31,6 +30,29 @@ function Gradient({
   );
 }
 
+const LINKS = [
+  {
+    title: "Docs",
+    href: "https://turbo.build/repo/docs",
+    description: "Find in-depth information about Turborepo features and API.",
+  },
+  {
+    title: "Learn",
+    href: "https://turbo.build/repo/docs/handbook",
+    description: "Learn more about monorepos with our handbook.",
+  },
+  {
+    title: "Templates",
+    href: "https://turbo.build/repo/docs/getting-started/from-example",
+    description: "Choose from over 15 examples and deploy with a single click.",
+  },
+  {
+    title: "Deploy",
+    href: "https://vercel.com/new",
+    description:
+        " Instantly deploy your Turborepo to a shareable URL with Vercel.",
+  },
+];
 export default function Page(): JSX.Element {
   return (
     <main className={styles.main}>
@@ -105,16 +127,13 @@ export default function Page(): JSX.Element {
           </div>
         </div>
       </div>
-      <ExtensionsProvider>
-        <ExtensionAccountsProvider
-          dappName={"test"}
-          network={"polkadex"}
-          ss58={88}
-        >
-          <div className={styles.grid}></div>
-          <TheButton />
-        </ExtensionAccountsProvider>
-      </ExtensionsProvider>
+      <div className={styles.grid}>
+        {LINKS.map(({ title, href, description }) => (
+            <Card className={styles.card} href={href} key={title} title={title}>
+              {description}
+            </Card>
+        ))}
+      </div>
     </main>
   );
 }
