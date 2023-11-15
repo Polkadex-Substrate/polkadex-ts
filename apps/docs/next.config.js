12c81b744dca654c061688a849646a98374c8a98
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2023-11-15 12:21:32 -0300

    update: reset docs package

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,20 +1,4 @@
-/** @type {import('next').NextConfig} */
-
-// eslint-disable-next-line prettier/prettier, @typescript-eslint/no-var-requires
-const withNextra = require("nextra")({
-  theme: "nextra-theme-docs",
-  themeConfig: "./src/theme.config.js",
-  defaultShowCopyCode: true,
-  staticImage: true,
-  latex: true,
-  flexsearch: {
-    codeblock: false,
-  },
-});
-
-const nextConfig = {
-  reactStrictMode: false,
-  transpilePackages: ["@polkadex/ux", "react-providers"],
+module.exports = {
+  reactStrictMode: true,
+  transpilePackages: ["ui", "react-providers"],
 };
-
-module.exports = withNextra(nextConfig);
--- apps/docs/src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import type { AppProps } from "next/app";
-import type { ReactElement } from "react";
-import "../styles/globals.scss";
-export default function Nextra({
-  Component,
-  pageProps,
-}: AppProps): ReactElement {
-  return <Component {...pageProps} />;
-}
