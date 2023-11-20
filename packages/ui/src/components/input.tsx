7fd29c8c40c55b3d65285ed0b6d4407855536e49
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2023-11-20 07:01:58 -0300

    update: new input passcode component and input vertical improvements

--- packages/ui/src/components/input.tsx
+++ packages/ui/src/components/input.tsx
@@ -1,7 +1,20 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
-import { InputHTMLAttributes, PropsWithChildren, useRef } from "react";
+import {
+  ChangeEvent,
+  FocusEvent,
+  InputHTMLAttributes,
+  KeyboardEvent,
+  MouseEvent,
+  PropsWithChildren,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import classNames from "classnames";
+import { twMerge } from "tailwind-merge";
 
 import { Button } from "./button";
+import { Typography } from "./typography";
 
 interface PrimaryInputProps extends InputHTMLAttributes<HTMLInputElement> {
   increase: () => void;
@@ -65,33 +78,156 @@ const Search = (props: InputHTMLAttributes<HTMLInputElement>) => (
 interface VerticalProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
   optional?: boolean;
-  action?: () => void;
+  action?: (e: MouseEvent<HTMLButtonElement>) => void;
+  actionTitle?: string;
 }
-const Vertical = ({ label, optional, action, ...props }: VerticalProps) => (
-  <div className="flex items-end justify-between gap-2 flex-1">
-    <div className="flex flex-col gap-2 flex-1">
-      <span className="text-primary">
-        {label} {optional && <span className="opacity-50">(Optional)</span>}
-      </span>
+const Vertical = ({
+  label,
+  optional,
+  action,
+  actionTitle,
+  id,
+  ...props
+}: VerticalProps) => (
+  <div className="flex flex-col gap-2 flex-1">
+    <Typography.Text variant="primary" size="xs">
+      {label} {optional && <span className="opacity-50">(Optional)</span>}
+    </Typography.Text>
+    <div className="flex items-center justify-between gap-2 flex-1">
       <input
+        id={id}
         type="text"
-        className="flex-1 bg-transparent text-md outline-none"
+        className="flex-1 bg-transparent text-lg outline-none font-semibold"
         {...props}
       />
+      {action && (
+        <Button.Solid appearance="secondary" size="sm" onClick={action}>
+          {actionTitle}
+        </Button.Solid>
+      )}
     </div>
-    {action && (
-      <Button.Primary
-        className="text-xs bg-secondary-hover p-1"
-        onClick={action}
-      >
-        Change
-      </Button.Primary>
-    )}
   </div>
 );
+
+interface PasscodeProps
+  extends Pick<InputHTMLAttributes<HTMLInputElement>, "className" | "type"> {
+  length?: number;
+  focusOnInit?: boolean;
+}
+
+const Passcode = ({
+  type,
+  length = 5,
+  focusOnInit,
+  className,
+}: PasscodeProps) => {
+  const inputsRef = useRef<Array<HTMLInputElement> | []>([]);
+
+  const [values, setValues] = useState<(string | number)[]>(
+    Array(length).fill("")
+  );
+  const [currentValue, setCurrentValue] = useState(0);
+
+  const onKeyUp = (e: KeyboardEvent<HTMLInputElement>, index: number) => {
+    const isBackspacePressed = e.key === "Backspace";
+    const hasRef = inputsRef && inputsRef?.current && index === currentValue;
+    const isValidNumber = !isNaN(parseInt(e.key)) && index <= values.length;
+
+    const updateCurrentValue = (newIndex: number) => {
+      setCurrentValue(newIndex);
+      if (hasRef) inputsRef.current[newIndex].focus();
+    };
+
+    setValues((prev) => {
+      const newArray = [...prev];
+
+      if (isBackspacePressed) {
+        if (index > 0 && newArray[index] !== undefined)
+          updateCurrentValue(index - 1);
+      } else if (isValidNumber) {
+        newArray[index] = parseInt(e.key) || e.key;
+        if (index < values.length - 1) {
+          updateCurrentValue(index + 1);
+        }
+      }
+
+      return newArray;
+    });
+  };
+
+  const onKeyDown = (e: KeyboardEvent<HTMLInputElement>, index: number) => {
+    const keyCode = Number(e.key);
+    const isNotBackspacePressed = e.key !== "Backspace";
+    const isRightArrowPressed = e.key === "ArrowRight";
+    const isLeftArrowPressed = e.key === "ArrowLeft";
+    const isTabPressed = e.key === "Tab";
+
+    const validNumber = !(keyCode >= 0 && keyCode <= 9);
+    const isPastePressed = !(e.metaKey && e.key === "v");
+
+    if (validNumber && isNotBackspacePressed && isPastePressed) {
+      e.preventDefault();
+    }
+    if ((isRightArrowPressed || isTabPressed) && index < values.length - 1) {
+      setCurrentValue(index + 1);
+      inputsRef.current[index + 1].focus();
+    }
+    if (isLeftArrowPressed && index > 0) {
+      setCurrentValue(index - 1);
+      inputsRef.current[index - 1].focus();
+    }
+  };
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>, index: number) =>
+    setValues((prev) => {
+      const newArray = [...prev];
+      const inputValue = e.target.value;
+      newArray[index] = parseInt(inputValue) || inputValue;
+      return newArray;
+    });
+
+  const onFocus = (e: FocusEvent<HTMLInputElement>, index: number) => {
+    setCurrentValue(index);
+    e.target.focus();
+  };
+
+  useEffect(() => {
+    if (focusOnInit && inputsRef && inputsRef.current)
+      inputsRef.current[0].focus();
+  }, [focusOnInit]);
+
+  return (
+    <div className="flex items-center gap-2">
+      {values.map((v, i) => (
+        <input
+          key={i}
+          ref={(element) => element && (inputsRef.current[i] = element)}
+          type={type}
+          maxLength={1}
+          inputMode="numeric"
+          pattern="\d{1}"
+          className={twMerge(
+            classNames(
+              "h-8 w-7 bg-level-3 text-center rounded-md transition-colors duration-300 border-2 border-transparent",
+              "focus:ring-0 focus:ring-offset-0 focus:border-primary-base hover:border-level-4",
+              values[i] && "bg-level-1"
+            ),
+            className
+          )}
+          value={String(v)}
+          onChange={(e) => onChange(e, i)}
+          onKeyUp={(e) => onKeyUp(e, i)}
+          onKeyDown={(e) => onKeyDown(e, i)}
+          onFocus={(e) => onFocus(e, i)}
+        />
+      ))}
+    </div>
+  );
+};
 
 export const Input = {
   Primary,
   Search,
   Vertical,
+  Passcode,
 };
