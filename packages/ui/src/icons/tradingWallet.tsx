07244820a2036afc991faa6b3322304341de0d1b
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2023-11-23 17:31:00 -0300

    update: trading wallet icon

---
+++ packages/ui/src/icons/tradingWallet.tsx
@@ -0,0 +1,55 @@
+import { ComponentProps } from "react";
+
+export function TradingWallet(props: ComponentProps<"svg">) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19 19" {...props}>
+      <path
+        d="M17.9278 9.9685V11.7047C17.9278 12.1682 17.5569 12.5475 17.085 12.5643H15.4331C14.5229 12.5643 13.6885 11.8985 13.6127 10.9883C13.5621 10.4573 13.7644 9.96009 14.1183 9.61454C14.4302 9.29428 14.86 9.10889 15.332 9.10889H17.085C17.5569 9.12574 17.9278 9.50497 17.9278 9.9685Z"
+        stroke="currentColor"
+        strokeWidth="0.73553"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M1.07178 10.146V7.88634C1.07178 5.59393 2.45396 3.99262 4.60309 3.72293C4.82221 3.68922 5.04977 3.67236 5.28575 3.67236H12.8709C13.09 3.67236 13.3007 3.68078 13.503 3.71449C15.6774 3.96733 17.0849 5.57708 17.0849 7.88634V9.1084H15.3318C14.8599 9.1084 14.4301 9.29379 14.1182 9.61405C13.7642 9.9596 13.562 10.4569 13.6126 10.9878C13.6884 11.898 14.5228 12.5638 15.433 12.5638H17.0849V13.7859C17.0849 16.3143 15.3993 17.9999 12.8709 17.9999H9.71191"
+        stroke="currentColor"
+        strokeWidth="0.73553"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M2.48389 11.8984L2.48389 17.6295"
+        stroke="currentColor"
+        strokeWidth="0.735904"
+      />
+      <path
+        d="M5.87988 11.8984L5.87988 17.6295"
+        stroke="currentColor"
+        strokeWidth="0.735904"
+      />
+      <rect
+        x="1.60796"
+        y="13.9791"
+        width="1.75215"
+        height="2.19019"
+        fill="currentColor"
+        stroke="currentColor"
+        strokeWidth="0.876077"
+      />
+      <rect
+        x="5.04107"
+        y="14.855"
+        width="1.75215"
+        height="0.876077"
+        fill="currentColor"
+        stroke="currentColor"
+        strokeWidth="0.876077"
+      />
+      <path
+        d="M4.67432 3.76105C4.67432 3.76105 8.26266 0.281642 10.3688 1.13462C11.8082 1.71758 12.121 3.76105 12.121 3.76105"
+        stroke="currentColor"
+        strokeWidth="0.735904"
+      />
+    </svg>
+  );
+}
