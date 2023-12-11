53ba360f8e1eb5ea3dc2e54e479e0d4ac2e4dda6
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2023-12-11 17:21:34 +0530

    style: lint fix

--- packages/types/src/chain.ts
+++ packages/types/src/chain.ts
@@ -1,12 +1,12 @@
-import { Struct } from '@polkadot/types';
-import { BN } from '@polkadot/util';
+import { Struct } from "@polkadot/types";
+import { BN } from "@polkadot/util";
 
 // blockchain account query result
 export interface SystemAccount extends Struct {
-    data: {
-        free: BN;
-        reserved: BN;
-        frozen: BN;
-        flags: BN;
-    };
+  data: {
+    free: BN;
+    reserved: BN;
+    frozen: BN;
+    flags: BN;
+  };
 }
