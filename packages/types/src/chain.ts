d81b6ee1f2242af055a193c928b0e060436446ea
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2023-12-11 16:29:45 +0530

    feat: add chain types

---
+++ packages/types/src/chain.ts
@@ -0,0 +1,12 @@
+import { Struct } from '@polkadot/types';
+import { BN } from '@polkadot/util';
+
+// blockchain account query result
+export interface SystemAccount extends Struct {
+    data: {
+        free: BN;
+        reserved: BN;
+        frozen: BN;
+        flags: BN;
+    };
+}
