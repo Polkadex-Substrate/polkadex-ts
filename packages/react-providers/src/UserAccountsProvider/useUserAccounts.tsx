23ab939fab513aed83b6f8b288ab1915ec646569
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2023-12-13 14:47:14 +0530

    fix: incorrect check in hook

--- packages/react-providers/src/UserAccountsProvider/useUserAccounts.tsx
+++ packages/react-providers/src/UserAccountsProvider/useUserAccounts.tsx
@@ -1,13 +1,10 @@
 import { useContext } from "react";
-
-import { ExtensionsContext } from "../ExtensionsProvider";
-
 import { UserAccountsContext } from "./provider";
 
 export const useUserAccounts = () => {
   const state = useContext(UserAccountsContext);
 
-  if (!ExtensionsContext) {
+  if (!UserAccountsContext) {
     const error = new Error("UserAccountsContext context is undefined");
     error.name = "ContextError";
     Error?.captureStackTrace?.(error, useContext);
