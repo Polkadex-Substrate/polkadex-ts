0927af26041896798e4290a06a76fc95093f06f0
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2023-12-13 14:43:23 +0530

    chore: add error if context not defined

--- packages/react-providers/src/ExtensionAccountsProvider/useExtensionAccounts.tsx
+++ packages/react-providers/src/ExtensionAccountsProvider/useExtensionAccounts.tsx
@@ -2,4 +2,15 @@ import { useContext } from "react";
 
 import { ExtensionAccountsContext } from "./provider";
 
-export const useExtensionAccounts = () => useContext(ExtensionAccountsContext);
+export const useExtensionAccounts = () => {
+  const state = useContext(ExtensionAccountsContext);
+
+  if (!ExtensionAccountsContext) {
+    const error = new Error("ExtensionAccountsContext context is undefined");
+    error.name = "ContextError";
+    Error?.captureStackTrace?.(error, useContext);
+    throw error;
+  }
+
+  return { ...state };
+};
--- packages/react-providers/src/ExtensionsProvider/useExtensions.tsx
+++ packages/react-providers/src/ExtensionsProvider/useExtensions.tsx
@@ -2,4 +2,15 @@ import { useContext } from "react";
 
 import { ExtensionsContext } from "./provider";
 
-export const useExtensions = () => useContext(ExtensionsContext);
+export const useExtensions = () => {
+  const state = useContext(ExtensionsContext);
+
+  if (!ExtensionsContext) {
+    const error = new Error("ExtensionsContext context is undefined");
+    error.name = "ContextError";
+    Error?.captureStackTrace?.(error, useContext);
+    throw error;
+  }
+
+  return { ...state };
+};
