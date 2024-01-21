30ba11f6f3eeca8f9f7ca4ad9555569acfdab27f
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2024-01-21 16:45:05 -0300

    feat: separator appearance

--- packages/ui/src/components/separator.tsx
+++ packages/ui/src/components/separator.tsx
@@ -2,28 +2,31 @@ import * as SeparatorRadix from "@radix-ui/react-separator";
 import classNames from "classnames";
 import { twMerge } from "tailwind-merge";
 
-const Horizontal = (
-  props: Omit<SeparatorRadix.SeparatorProps, "orientation">
-) => {
+import { themeConfig } from "../../../../themeConfig";
+
+const Horizontal = (props: Omit<SeparatorProps, "orientation">) => {
   return <Base orientation="horizontal" {...props} />;
 };
 
-const Vertical = (
-  props: Omit<SeparatorRadix.SeparatorProps, "orientation">
-) => {
+const Vertical = (props: Omit<SeparatorProps, "orientation">) => {
   return <Base orientation="vertical" {...props} />;
 };
+interface SeparatorProps extends SeparatorRadix.SeparatorProps {
+  appearance?: keyof typeof themeConfig.theme.extend.backgroundColor;
+}
 
 const Base = ({
   orientation,
   className,
+  appearance = "level-3",
   ...props
-}: SeparatorRadix.SeparatorProps) => {
+}: SeparatorProps) => {
+  const bgColor = `bg-${appearance}`;
   return (
     <SeparatorRadix.Root
       className={twMerge(
         classNames(
-          "flex-1 bg-level-3",
+          bgColor,
           orientation === "horizontal" ? "h-px w-full" : "w-px h-full"
         ),
         className
