134a1153b538d9b7cd03d9c7632c48425de0ac3e
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2024-01-22 17:38:56 -0300

    fix: improve table styles

--- packages/ui/src/components/table.tsx
+++ packages/ui/src/components/table.tsx
@@ -4,6 +4,8 @@ import { ComponentProps, PropsWithChildren } from "react";
 import classNames from "classnames";
 import { twMerge } from "tailwind-merge";
 
+import { Typography } from "./typography";
+
 const Caption = ({
   children,
   className,
@@ -12,7 +14,10 @@ const Caption = ({
   return (
     <caption
       className={twMerge(
-        classNames("mt-4 text-sm caption-bottom text-primary", className)
+        classNames(
+          "p-4 text-sm caption-bottom text-primary opacity-50 border-t border-secondary"
+        ),
+        className
       )}
       {...props}
     >
@@ -21,42 +26,77 @@ const Caption = ({
   );
 };
 
+type CellProps = ComponentProps<"td"> & AlignProps;
+
 const Cell = ({
   children,
   className,
+  align = "left",
   ...props
-}: PropsWithChildren<ComponentProps<"td">>) => {
+}: PropsWithChildren<CellProps>) => {
+  const isString = typeof children === "string";
   return (
     <td
       className={twMerge(
         classNames(
-          "text-sm font-normal align-middle [&:has([role=checkbox])]:pr-0 px-2 py-3",
-          className
-        )
+          "[&:has([role=checkbox])]:pr-0 p-2",
+          `text-${align}`,
+          !isString && className
+        ),
+        className
       )}
-      {...props}
+      {...(!isString && props)}
     >
-      {children}
+      {isString ? (
+        <Typography.Text
+          size="sm"
+          className={twMerge(classNames("font-normal"), className)}
+          {...props}
+        >
+          {children}
+        </Typography.Text>
+      ) : (
+        children
+      )}
     </td>
   );
 };
 
+type AlignProps = {
+  align?: "left" | "center" | "right";
+};
+
+type HeadProps = ComponentProps<"th"> & AlignProps;
 const Head = ({
   children,
   className,
+  align = "left",
   ...props
-}: PropsWithChildren<ComponentProps<"th">>) => {
+}: PropsWithChildren<HeadProps>) => {
+  const isString = typeof children === "string";
   return (
     <th
       className={twMerge(
         classNames(
-          "text-sm font-normal text-left align-middle text-primary [&:has([role=checkbox])]:pr-0 p-2 border-b border-secondary",
-          className
+          "[&:has([role=checkbox])]:pr-0 p-2 ",
+          `text-${align}`,
+          !isString && className
         )
       )}
-      {...props}
+      {...(!isString && props)}
     >
-      {children}
+      {isString ? (
+        <Typography.Text
+          appearance="primary"
+          size="xs"
+          className={twMerge(classNames("font-normal"), className)}
+          {...props}
+        >
+          {children}
+        </Typography.Text>
+      ) : (
+        children
+      )}
     </th>
   );
 };
@@ -70,9 +110,9 @@ const Row = ({
     <tr
       className={twMerge(
         classNames(
-          "transition-colors hover:bg-level-0 data-[state=selected]:bg-level-2",
-          className
-        )
+          "transition-colors hover:bg-level-0 data-[state=selected]:bg-level-2"
+        ),
+        className
       )}
       {...props}
     >
@@ -83,72 +123,57 @@ const Row = ({
 
 const Body = ({
   children,
-  className,
   ...props
 }: PropsWithChildren<ComponentProps<"tbody">>) => {
-  return (
-    <tbody
-      className={twMerge(
-        classNames(
-          "[&_tr:last-child]:border-none border-b border-secondary",
-          className
-        )
-      )}
-      {...props}
-    >
-      {children}
-    </tbody>
-  );
+  return <tbody {...props}>{children}</tbody>;
 };
 
 const Header = ({
   children,
-  className,
   ...props
 }: PropsWithChildren<ComponentProps<"thead">>) => {
-  return (
-    <thead
-      className={twMerge(
-        classNames("[&_tr]:border-b border-secondary", className)
-      )}
-      {...props}
-    >
-      {children}
-    </thead>
-  );
+  return <thead {...props}>{children}</thead>;
 };
 
 const Footer = ({
   children,
-  className,
   ...props
 }: PropsWithChildren<ComponentProps<"tfoot">>) => {
-  return (
-    <tfoot
-      className={twMerge(
-        classNames("border-t [&>tr]:last:border-b-none bg-level-0", className)
-      )}
-      {...props}
-    >
-      {children}
-    </tfoot>
-  );
+  return <tfoot {...props}>{children}</tfoot>;
+};
+
+interface TableProps extends ComponentProps<"table"> {
+  variant?: keyof typeof variantsStyles;
+  even?: boolean;
+}
+
+const variantsStyles = {
+  line: "[&_tr:not(:last-child)]:border-b [&_tr]:border-primary [&_th]:border-b [&_th]:border-secondary",
+  modern:
+    "[&_th]:bg-level-0 [&_th:first-child]:rounded-l-lg [&_th:last-child]:rounded-r-lg",
 };
 
 const Table = ({
   children,
   className,
+  variant,
+  even,
   ...props
-}: PropsWithChildren<ComponentProps<"table">>) => {
+}: PropsWithChildren<TableProps>) => {
   return (
-    <div className="relative w-full overflow-auto">
-      <table
-        className={twMerge(classNames("w-full text-sm", className))}
-        {...props}
-      >
-        {children}
-      </table>
-    </div>
+    <table
+      className={twMerge(
+        classNames(
+          "w-full overflow-auto border-spacing-0 border-collapse",
+          variant && variantsStyles[variant],
+          even && "even:[&_tr]:bg-level-0"
+        ),
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </table>
   );
 };
 
