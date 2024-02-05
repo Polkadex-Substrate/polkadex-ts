e1bf252e1299ce1659dc21039e490abf28945034
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2024-02-05 18:55:55 +0530

    feat: add extrinincs for swap

--- packages/polkadex-api/src/modules/swap.ts
+++ packages/polkadex-api/src/modules/swap.ts
@@ -1,3 +1,4 @@
+import { SubmittableExtrinsic } from "@polkadot/api/promise/types";
 import { cleanNumberLike, toPlank, toUnit } from "@polkadex/numericals";
 import { Option, u128 } from "@polkadot/types";
 
@@ -84,5 +85,111 @@ export class SwapApi extends BaseApi {
         includeFee
       );
     return toUnit(result.unwrap().toString(), this.chainDecimals).toNumber();
+  }
+
+  createAssetIdEnum(id: string | object): Record<string, null | string> {
+    if (!!id && typeof id === "object") {
+      return id;
+    }
+    if (id.toUpperCase() === "POLKADEX" || id.toUpperCase() === "PDEX") {
+      return { polkadex: null };
+    }
+    return { asset: id };
+  }
+
+  public async addLiquidityTx(
+    base: string,
+    quote: string,
+    amountBaseDesired: number,
+    amountQuoteDesired: number,
+    amountBaseMin: number,
+    amountQuoteMin: number,
+    mintToAddress: string
+  ): Promise<SubmittableExtrinsic> {
+    const asset1 = this.createAssetIdEnum(base);
+    const asset2 = this.createAssetIdEnum(quote);
+    const amount1Desired = toPlank(amountBaseDesired).toFixed();
+    const amount2Desired = toPlank(amountQuoteDesired).toFixed();
+    const amount1Min = toPlank(amountBaseMin).toFixed();
+    const amount2Min = toPlank(amountQuoteMin).toFixed();
+    return this.api.tx.assetConversion.addLiquidity(
+      asset1,
+      asset2,
+      amount1Desired,
+      amount2Desired,
+      amount1Min,
+      amount2Min,
+      mintToAddress
+    );
+  }
+
+  public async createPoolTx(
+    base: string,
+    quote: string
+  ): Promise<SubmittableExtrinsic> {
+    const asset1 = this.createAssetIdEnum(base);
+    const asset2 = this.createAssetIdEnum(quote);
+    return this.api.tx.assetConversion.createPool(asset1, asset2);
+  }
+
+  public async removeLiquidityTx(
+    base: string,
+    quote: string,
+    lpTokenBurnAmout: string,
+    amountBaseMinRecive: string,
+    amountQuoteMinrecive: string,
+    withdrawTo: string
+  ): Promise<SubmittableExtrinsic> {
+    const asset1 = this.createAssetIdEnum(base);
+    const asset2 = this.createAssetIdEnum(quote);
+    const lpTokenBurn = toPlank(lpTokenBurnAmout).toFixed();
+    const amount1Min = toPlank(amountBaseMinRecive).toFixed();
+    const amount2Min = toPlank(amountQuoteMinrecive).toFixed();
+    return this.api.tx.assetConversion.removeLiquidity(
+      asset1,
+      asset2,
+      lpTokenBurn,
+      amount1Min,
+      amount2Min,
+      withdrawTo
+    );
+  }
+
+  public async swapExactTokensForTokensTx(
+    path: string[],
+    amountIn: number,
+    amountOutMin: number,
+    toAddress: string,
+    keepAlive = true
+  ): Promise<SubmittableExtrinsic> {
+    const assetPath = path.map((asset) => this.createAssetIdEnum(asset));
+    const amtIn = toPlank(amountIn, this.chainDecimals).toFixed();
+    const amtOut = toPlank(amountOutMin, this.chainDecimals).toFixed();
+    return this.api.tx.swapExactTokensForTokens(
+      assetPath,
+      amtIn,
+      amtOut,
+      toAddress,
+      keepAlive
+    );
+  }
+
+  public async swapTokensForExactTokens(
+    path: string[],
+    amountOut: number,
+    amountInMax: number,
+    toAddress: string,
+    keepAlive = true
+  ): Promise<SubmittableExtrinsic> {
+    const assetPath = path.map((asset) => this.createAssetIdEnum(asset));
+    const amtOut = toPlank(amountOut, this.chainDecimals).toFixed();
+    const amtIn = toPlank(amountInMax, this.chainDecimals).toFixed();
+    return this.api.tx.swapExactTokensForTokens(
+      assetPath,
+      amtOut,
+      amtIn,
+      toAddress,
+      keepAlive
+    );
   }
 }
