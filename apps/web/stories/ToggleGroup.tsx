6e6f613580d517afa6de0e740f65a140a1a07b77
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2024-02-08 14:54:47 -0300

    feat: togglegroup story

---
+++ apps/web/stories/ToggleGroup.tsx
@@ -0,0 +1,15 @@
+// import { ToggleGroup as PolkadeToggleGroup } from "@polkadex/ux";
+import { Fragment } from "react";
+
+export const ToggleGroup = () => {
+  return <Fragment>test</Fragment>;
+};
+
+// export const ToggleGroup = () => {
+//   return (
+//     <PolkadeToggleGroup type="single">
+//       <PolkadeToggleGroup.Item value="all">All</PolkadeToggleGroup.Item>
+//       <PolkadeToggleGroup.Item value="recent">Recent</PolkadeToggleGroup.Item>
+//     </PolkadeToggleGroup>
+//   );
+// };
