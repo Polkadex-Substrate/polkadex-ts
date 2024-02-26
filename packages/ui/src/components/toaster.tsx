e792f06bc7275a72311d2a684886fe73ebc515f9
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2024-02-26 07:38:27 -0300

    fix: closeButton styles

--- packages/ui/src/components/toaster.tsx
+++ packages/ui/src/components/toaster.tsx
@@ -17,10 +17,12 @@ export const Toaster = ({ theme, ...props }: ComponentProps<typeof Sonner>) => {
             "group-[.toast]:bg-primary-base group-[.toast]:hover:bg-primary-hover group-[.toast]:active:bg-primary-pressed group-[.toast]:transition-colors group-[.toast]:duration-300 group-[.toast]:text-medium group-[.toast]:text-xs",
           cancelButton:
             "group-[.toast]:bg-secondary-base group-[.toast]:text-primary group-[.toast]:hover:text-white group-[.toast]:hover:bg-secondary-hover group-[.toast]:active:bg-secondary-pressed group-[.toast]:transition-colors group-[.toast]:duration-300 group-[.toast]:text-medium group-[.toast]:text-xs",
-          success: "[&_svg]:text-success-base",
-          warning: "[&_svg]:text-attention-base",
-          error: "[&_svg]:text-danger-base",
-          info: "[&_svg]:text-info-base",
+          closeButton:
+            "group-[.toast]:bg-tertiary-base group-[.toast]:hover:!bg-secondary-hover [&_svg]:text-white group-[.toast]:border-none group-[.toast]:top-1 group-[.toast]:left-[98%]",
+          success: "[&>[data-icon]]:text-success-base",
+          warning: "[&>[data-icon]]:text-attention-base",
+          error: "[&>[data-icon]]:text-danger-base",
+          info: "[&>[data-icon]]:text-info-base",
         },
       }}
       {...props}
