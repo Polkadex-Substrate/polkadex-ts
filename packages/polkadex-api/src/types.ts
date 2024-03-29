29f62a889b61060b1fa60fefdd741cc452e81b15
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2024-03-29 21:43:51 +0530

    feat: add signedExtensions

--- packages/polkadex-api/src/types.ts
+++ packages/polkadex-api/src/types.ts
@@ -259,4 +259,14 @@ export const rpc = {
   },
 };
 
-export const apiTypes = { types, rpc, runtime };
+export const signedExtensions = {
+  ChargeAssetTxPayment: {
+    extrinsic: {
+      tip: "Compact<Balance>",
+      assetId: "Option<u128>",
+    },
+    payload: {},
+  },
+};
+
+export const apiTypes = { types, rpc, runtime, signedExtensions };
