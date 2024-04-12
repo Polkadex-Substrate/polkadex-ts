7622a3c0b97bcebe61c8e02e95231e4b6a3bbc18
Arjun Porwal <65214523+ap211unitech@users.noreply.github.com>
2024-04-12 12:44:38 +0530

    created: crossChain transfer fn

--- apps/cross-chain-transfer/app/page.tsx
+++ apps/cross-chain-transfer/app/page.tsx
@@ -1,21 +1,51 @@
-import {} from "@polkadex/thea";
+"use client";
+
+import { ParachainTransferService } from "@polkadex/thea";
 
 const fromChain = "assethub";
 const toChain = "polkadex";
 
-const senderAddr = "15GYTpDWPEvbnkDqJeyqNQREKXUfXPcAvK8wJP48QrmAW8wL";
-const destinationAddr = "esq2wFkRsic8WM4nstAtkjqWdCDnTrGHMpFjaGN2rEHnQXUNm";
+const sourceAddress = "15GYTpDWPEvbnkDqJeyqNQREKXUfXPcAvK8wJP48QrmAW8wL";
+const destinationAddress = "esq2wFkRsic8WM4nstAtkjqWdCDnTrGHMpFjaGN2rEHnQXUNm";
 const amount = 0.001;
+const asset = "usdt";
 
 export default function Home() {
-  const crossChainTransfer = async () => {};
+  const crossChainTransfer = async () => {
+    const parachainTransfer = new ParachainTransferService();
+
+    const { web3Enable, web3FromAddress } = await import(
+      "@polkadot/extension-dapp"
+    );
+
+    await web3Enable("polkadex-thea");
+    const { signer } = await web3FromAddress(sourceAddress);
+    if (!signer) throw new Error("Polkdot signer not found");
+
+    const transferData = await parachainTransfer.getTransferData({
+      sourceKeyOrChain: fromChain,
+      sourceAddress,
+
+      destinationAddress,
+      destinationKeyOrChain: toChain,
+
+      keyOrAsset: asset,
+      polkadotSigner: signer,
+    });
+
+    console.log("Transfer data => ", transferData);
+  };
 
   return (
     <div className="flex flex-col items-center justify-center">
       <h1 className="text-3xl text-center my-4 text-blue-500 underline underline-offset-[5px] decoration-dashed">
-        XCM transfer with Paraspell {"<>"} PoC
+        XCM transfer
       </h1>
-      <p className="text-lg">Sending AssetHub to Polkadex for {amount} USDT</p>
+      <p className="text-lg">
+        Sending from <span className="capitalize">{fromChain}</span> to{" "}
+        <span className="capitalize">{toChain}</span> for {amount}{" "}
+        <span className="uppercase">{asset}</span>
+      </p>
       <button
         className="bg-yellow-50 text-black px-4 py-2 my-4"
         onClick={crossChainTransfer}
