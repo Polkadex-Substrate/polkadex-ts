dcb76fffd74fcb63692dc769742e1662b5a9e566
Arjun Porwal <65214523+ap211unitech@users.noreply.github.com>
2024-04-30 09:20:24 +0530

    feat: withdraw tested for Astar - ASTR

--- packages/thea/src/config/substrate/config/polkadex.ts
+++ packages/thea/src/config/substrate/config/polkadex.ts
@@ -149,14 +149,15 @@ const toAstar: AssetConfig[] = [
     balance: BalanceBuilder().substrate().assets().account(),
     destination: astar,
     destinationFee: {
-      amount: 0,
+      amount: 0, // Not sure
       asset: astr,
       balance: BalanceBuilder().substrate().system().account(),
     },
     extrinsic: ExtrinsicBuilderV2()
-      .polkadotXcm()
-      .reserveTransferAssets()
-      .here(),
+      .theaExecuter()
+      .parachainWithdraw()
+      .X2()
+      .sufficient(),
     min: AssetMinBuilder().assets().asset(),
     fee: {
       asset: pdex,
