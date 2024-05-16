ad31b03185f887d93d74ce36d49ffe8f8d91a6af
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2024-05-16 06:46:30 -0300

    fix: improve providerCard readyToUse component

--- packages/ui/src/readyToUse/index.ts
+++ packages/ui/src/readyToUse/index.ts
@@ -4,3 +4,5 @@ export * from "./accountCard";
 export * from "./transactionLoading";
 export * from "./responsiveCard";
 export * from "./hoverInformation";
+export * from "./genericHorizontalItem";
+export * from "./providerCard";
