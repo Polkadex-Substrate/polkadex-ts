989e02daeee32e1e1d370f86c4055dce8988e4b7
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2024-05-17 01:32:12 -0300

    fix: Token props

--- packages/ui/src/components/icon.tsx
+++ packages/ui/src/components/icon.tsx
@@ -11,12 +11,17 @@ import {
   sizesVariants,
 } from "../helpers";
 
+import { Skeleton } from "./skeleton";
+
 export type IconsProps = keyof typeof Icons;
 
 interface Props extends ComponentProps<"div"> {
   name?: IconsProps;
   appearance?: AppearanceBackgroundVariants;
   size?: SizesVariants;
+  bordered?: boolean;
+  rounded?: boolean;
+  loading?: boolean;
 }
 
 export const Icon = ({
@@ -24,6 +29,9 @@ export const Icon = ({
   size = "md",
   className,
   appearance,
+  loading,
+  bordered,
+  rounded,
   children,
   ...props
 }: PropsWithChildren<Props>) => {
@@ -31,18 +39,29 @@ export const Icon = ({
   const IconComponent = Icons[initialName];
 
   return (
-    <div
-      className={twMerge(
-        classNames(
-          "flex items-center justify-center",
-          sizesVariants[size],
-          appearance && `bg-${appearance}`
-        ),
-        className
+    <Skeleton
+      loading={loading}
+      className={classNames(
+        "flex-none",
+        sizesVariants[size],
+        rounded && "rounded-full"
       )}
-      {...props}
     >
-      {children || <IconComponent className="w-full h-full" />}
-    </div>
+      <div
+        className={twMerge(
+          classNames(
+            "flex items-center justify-center",
+            sizesVariants[size],
+            appearance && `bg-${appearance}`,
+            bordered && "border border-secondary",
+            rounded && "rounded-full"
+          ),
+          className
+        )}
+        {...props}
+      >
+        {children || <IconComponent className="w-full h-full" />}
+      </div>
+    </Skeleton>
   );
 };
